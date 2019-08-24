815f30b402ea57e28b8f699e834a43583e0b22b8
Glassinatorn <gusten1995@gmail.com>
2019-08-24 17:37:51 +0200

    Updated shutdown script

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -7,11 +7,12 @@
 #	e   88 88  8 88  8   88  88  8 8   8 88  8  8 88  8
 #	8eee88 88  8 88ee8   88  88ee8 8eee8 88ee8ee8 88  8
 
-ANS=`echo -e "Lock \nSwitch \nReboot \nShutdown" | dmenu_center -p Power`
+ANS=`echo -e "Lock \nLog out \nSwitch \nReboot \nShutdown" | dmenu_center -p Power`
 
 case $ANS in
 	"Lock ") dm-tool switch-to-greeter ;;
 	"Reboot ") reboot ;;
+	"Log out ") killall dwm ;;
 	"Switch ") dm-tool switch-to-greeter ;;
 	"Shutdown") shutdown now ;;
 esac
