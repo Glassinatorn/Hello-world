301b91bfe0af1217a75717e223f9e3d4ec081be9
Glassinatorn <gusten1995@gmail.com>
2019-10-10 12:27:45 +0200

    Fixed egpu script to unload and load nvidia driver

--- bin/egpu.sh
+++ bin/egpu.sh
@@ -11,11 +11,13 @@
 DIR="/etc/X11/xorg.conf.d"
 
 if [ `ls $DIR/ | grep 20` == "20-nvidia.conf" ]; then
-	sudo mv $DIR/../20-intel.conf $DIR/ && \
-	sudo mv $DIR/20-nvidia.conf $DIR/../20-nvidia.conf
+    sudo mv $DIR/../20-intel.conf $DIR/ && \
+    sudo mv $DIR/20-nvidia.conf $DIR/../20-nvidia.conf
 else
-	sudo mv $DIR/../20-nvidia.conf $DIR/ && \
-	sudo mv $DIR/20-intel.conf $DIR/../20-intel.conf
+    sudo modprobe -r nvidia-drm
+    sudo modprobe nvidia-drm
+    sudo mv $DIR/../20-nvidia.conf $DIR/ && \
+    sudo mv $DIR/20-intel.conf $DIR/../20-intel.conf
 fi
 
 sudo systemctl restart lightdm
