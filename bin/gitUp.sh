49a3c33213f9d445c448c00168c72500f52d7990
Glassinatorn <gusten1995@gmail.com>
2020-08-10 23:58:33 +0200

    Reformatted gitUp script

--- bin/gitUp.sh
+++ bin/gitUp.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-
 #     @@@@@@@@  @@@  @@@@@@@  @@@  @@@  @@@@@@@
 #    @@@@@@@@@  @@@  @@@@@@@  @@@  @@@  @@@@@@@@
 #    !@@        @@!    @@!    @@!  @@@  @@!  @@@
@@ -12,79 +11,92 @@
 #     ::: ::::   ::     ::    ::::: ::   ::
 #     :: :: :   :       :      : :  :    :
 
-
-CONFDIR="$HOME/.config"
-BINDIR="$HOME/.local/bin/own"
-NVIMRUNTIME="/usr/share/nvim/runtime"
-BACKUPFOLDER="$HOME/freetime/code/various/Hello-world"
-SUCKLESS="$HOME/freetime/code/c/git/suckless"
+CD="$HOME/.config"
+BID="$HOME/.local/bin/own"
+ND="/usr/share/nvim/runtime"
+BD="$HOME/freetime/code/various/Hello-world"
+SD="$HOME/freetime/code/c/git/suckless"
 CMD="sudo rsync -razzP --del"
 
 # .config
-$CMD $CONFDIR/i3 \
-     $CONFDIR/bspwm \
-     $CONFDIR/awesome \
-     $CONFDIR/sxhkd \
-     $CONFDIR/gtk-3.0 \
-     $CONFDIR/polybar \
-     $CONFDIR/termite \
-     $CONFDIR/picom.conf	        $BACKUPFOLDER/config/
+$CMD $CD/i3 \
+    $CD/bspwm \
+    $CD/awesome \
+    $CD/sxhkd \
+    $CD/gtk-3.0 \
+    $CD/polybar \
+    $CD/termite \
+    $CD/picom.conf \
+    $BD/config/
 
 # nnn
-$CMD $CONFDIR/nnn/plugins \
-     $CONFDIR/nnn/rc                    $BACKUPFOLDER/config/nnn/
+$CMD $CD/nnn/plugins \
+    $CD/nnn/rc \
+    $BD/config/nnn/
 
 # qutebrowser
-$CMD $CONFDIR/qutebrowser/config.py     $BACKUPFOLDER/config/qutebrowser/
+$CMD $CD/qutebrowser/config.py \
+    $BD/config/qutebrowser/
 
 # tmux
-$CMD /etc/tmux.conf                     $BACKUPFOLDER/etc/
+$CMD /etc/tmux.conf \
+    $BD/etc/
 
 # zsh
-$CMD $HOME/.zshrc                        $BACKUPFOLDER/zshrc
+$CMD $HOME/.zshrc \
+    $BD/zshrc
 
 # profiles
-$CMD $HOME/.xprofile                     $BACKUPFOLDER/xprofile
-$CMD $HOME/.profile                      $BACKUPFOLDER/profile
+$CMD $HOME/.xprofile \
+    $BD/xprofile
+$CMD $HOME/.profile \
+    $BD/profile
 
 # xorg
 $CMD /etc/X11/xorg.conf.d/20-* \
-     /etc/X11/20-*                      $BACKUPFOLDER/etc/X11/
+    /etc/X11/20-* \
+    $BD/etc/X11/
 
 # bin
-$CMD $BINDIR/fix \
-     $BINDIR/egpu.sh \
-     $BINDIR/gitUp.sh \
-     $BINDIR/sysinfo \
-     $BINDIR/shutdown.sh \
-     $BINDIR/keymap \
-     $BINDIR/qutebrowser_session \
-     $BINDIR/shutdown.sh \
-     $BINDIR/brightness \
-     $BINDIR/rotate.sh                  $BACKUPFOLDER/bin/
+$CMD $BID/fix \
+    $BID/egpu.sh \
+    $BID/gitUp.sh \
+    $BID/sysinfo \
+    $BID/shutdown.sh \
+    $BID/keymap \
+    $BID/qutebrowser_session \
+    $BID/shutdown.sh \
+    $BID/brightness \
+    $BID/rotate.sh \
+    $BD/bin/
 
 # suckless
-$CMD --exclude '.git' $SUCKLESS/dmenu   $BACKUPFOLDER/suckless/
-$CMD --exclude '.git' $SUCKLESS/dwm     $BACKUPFOLDER/suckless/
-$CMD --exclude '.git' $SUCKLESS/st      $BACKUPFOLDER/suckless/
+$CMD --exclude '.git' $SD/dmenu \
+    $BD/suckless/
+$CMD --exclude '.git' $SD/dwm \
+    $BD/suckless/
+$CMD --exclude '.git' $SD/st \
+    $BD/suckless/
 
 # nvim
-$CMD $CONFDIR/nvim/init.vim \
-     $CONFDIR/nvim/coc-settings.json    $BACKUPFOLDER/nvim/
-$CMD $CONFDIR/nvim/after/               $BACKUPFOLDER/nvim/after/
+$CMD $CD/nvim/init.vim \
+    $CD/nvim/coc-settings.json \
+    $BD/nvim/
+$CMD $CD/nvim/after/ \
+    $BD/nvim/after/
 
-$CMD $NVIMRUNTIME/plugin/map_*          $BACKUPFOLDER/nvim/plugin/
-$CMD $NVIMRUNTIME/colors/mono_tl.vim*   $BACKUPFOLDER/nvim/colors/
-$CMD $NVIMRUNTIME/skeletons/*           $BACKUPFOLDER/nvim/skeletons/
-$CMD $NVIMRUNTIME/doc/doc.txt \
-     $NVIMRUNTIME/doc/doc_*             $BACKUPFOLDER/nvim/doc/
+$CMD $ND/plugin/map_* $BD/nvim/plugin/
+$CMD $ND/colors/mono_tl.vim* $BD/nvim/colors/
+$CMD $ND/skeletons/* $BD/nvim/skeletons/
+$CMD $ND/doc/doc.txt \
+    $ND/doc/doc_* \
+    $BD/nvim/doc/
 
 echo "upload? y?"
 read UPLOAD
 
-if [ $UPLOAD = 'y' ]
-then
-    cd $BACKUPFOLDER
+if [ $UPLOAD = 'y' ]; then
+    cd $BD
     git add *
     git commit
     git push
