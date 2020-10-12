354412555a0f091bba29c329208bf4614639e7da
glassinatorn <gusten1995@gmail.com>
2020-10-12 02:29:25 +0200

    Updated structure and added comment

--- bin/qutebrowser_session.sh
+++ bin/qutebrowser_session.sh
@@ -13,7 +13,9 @@
 #    :: : :    : :: ::   :: : :    :: : :    :     : :  :   ::    :
 
 
-SESSION=$(ls ~/.local/share/qutebrowser/sessions | cut -d '.' -f 1 | \
-          dmenu -c -l 10 -p Qutebrowser_session)
+# getting the session to load into
+SESSION=$(ls ~/.local/share/qutebrowser/sessions \
+    | cut -d '.' -f 1 \
+    | dmenu -c -l 10 -p Qutebrowser_session)
 
 qutebrowser -r ~/.local/share/qutebrowser/sessions/$SESSION.yml
