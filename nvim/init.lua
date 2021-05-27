5b3fd3182087e0f3893c023bd4ef88b45d24399f
Glassinatorn <gusten1995@gmail.com>
2021-05-27 19:05:35 +0200

    Added popfix for lsputils

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,6 +28,8 @@ paq {'glassinatorn/figlet-banner'}
 paq {'christianchiarulli/nvcode-color-schemes.vim'}
 paq {'puremourning/vimspector'}
 paq {'kabouzeid/nvim-lspinstall'}
+paq {'RishabhRD/popfix'}
+paq {'RishabhRD/nvim-lsputils'}
 
 
 -- loading settings
@@ -36,3 +38,12 @@ require('maps')                         -- keybindings
 require('lsp_config')                   -- config for lsp
 require('treesitter_config')            -- treesitter config
 require('compe_config')                 -- completion with compe
+
+vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
+vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
+vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
+vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
+vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
+vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
+vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
+vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
