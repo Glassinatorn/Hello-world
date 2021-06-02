e7cac61324e50564bc458303f91f03b7e7a6e443
glassinatorn <gusten1995@gmail.com>
2021-06-02 17:45:42 +0200

    Formatted the file

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -5,43 +5,43 @@ local map = vim.api.nvim_set_keymap
 g.mapleader = ' '
 
 -- lsp actions
-map('n', '<leader>h', ':lua vim.lsp.diagnostic.goto_prev()<CR>',    { noremap = true })
-map('n', '<leader>l', ':lua vim.lsp.diagnostic.goto_next()<CR>',    { noremap = true })
-map('n', '<leader>a', ':lua vim.lsp.buf.code_action()<CR>',	    { noremap = true })
-map('n', '<leader>d', ':lua vim.lsp.buf.definition()<CR>',	    { noremap = true })
-map('n', '<leader>D', ':lua vim.lsp.buf.declaration()<CR>',	    { noremap = true })
-map('n', '<leader>r', ':lua vim.lsp.buf.references()<CR>',	    { noremap = true })
-map('n', '<leader>i', ':lua vim.lsp.buf.implementation()<CR>',	    { noremap = true })
-map('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>',		    { noremap = true })
-map('n', 'K', ':lua vim.lsp.buf.hover()<CR>',			    { noremap = true })
+map('n', '<leader>h',       ':lua vim.lsp.diagnostic.goto_prev()<CR>',  { noremap = true })
+map('n', '<leader>l',       ':lua vim.lsp.diagnostic.goto_next()<CR>',  { noremap = true })
+map('n', '<leader>a',       ':lua vim.lsp.buf.code_action()<CR>',	{ noremap = true })
+map('n', '<leader>d',       ':lua vim.lsp.buf.definition()<CR>',	{ noremap = true })
+map('n', '<leader>D',       ':lua vim.lsp.buf.declaration()<CR>',	{ noremap = true })
+map('n', '<leader>r',       ':lua vim.lsp.buf.references()<CR>',	{ noremap = true })
+map('n', '<leader>i',       ':lua vim.lsp.buf.implementation()<CR>',	{ noremap = true })
+map('n', '<leader>rn',      ':lua vim.lsp.buf.rename()<CR>',		{ noremap = true })
+map('n', 'K',               ':lua vim.lsp.buf.hover()<CR>',		{ noremap = true })
 
 -- nice to haves
-map('i', 'kj', '<Esc>',			                            { noremap = true })	    -- escape insert
-map('i', 'jk', '<Esc>',	    		                            { noremap = true })	    -- escape insert
-map('i', '()', '()<Esc>i',	    	                            { noremap = true })	    -- insert ()
-map('i', '[]', '[]<Esc>i',	    	                            { noremap = true })	    -- insert []
-map('i', '{}', '{}<Esc>i',	    	                            { noremap = true })	    -- insert {}
-map('i', '<>', '<><Esc>i',	    	                            { noremap = true })	    -- insert <>
-map('i', '""', '""<Esc>i',	    	                            { noremap = true })	    -- insert ""
-map('i', '\'\'', '\'\'<Esc>i',	    	                            { noremap = true })	    -- insert ""
-map('n', 'zz', 'z=',	    		                            { noremap = true })	    -- fix spelling
-map('n', 'z<space>', 'za',  		                            { noremap = true })	    -- toggle fold
-map('i', '<C-s>', '<Esc>:w<CR>',	                            { noremap = true })	    -- save with control-s
-map('n', '<C-s>', ':w<CR>',		                            { noremap = true })	    -- save with control-s
-map('n', '<leader>fo', ':Neoformat<CR>',                            { noremap = true })	    -- formatting
-map('n', '<leader>m', ':MaximizerToggle<CR>',                       { noremap = true })	    -- maximized buffer
+map('i', 'kj',              '<Esc>',			                { noremap = true })	-- escape insert
+map('i', 'jk',              '<Esc>',	    		                { noremap = true })	-- escape insert
+map('i', '()',              '()<Esc>i',	    	                        { noremap = true })	-- insert ()
+map('i', '[]',              '[]<Esc>i',	    	                        { noremap = true })	-- insert []
+map('i', '{}',              '{}<Esc>i',	    	                        { noremap = true })	-- insert {}
+map('i', '<>',              '<><Esc>i',	    	                        { noremap = true })	-- insert <>
+map('i', '""',              '""<Esc>i',	    	                        { noremap = true })	-- insert ""
+map('i', '\'\'',            '\'\'<Esc>i',	    	                { noremap = true })	-- insert ""
+map('n', 'zz',              'z=',	    		                { noremap = true })	-- fix spelling
+map('n', 'z<space>',        'za',  		                        { noremap = true })	-- toggle fold
+map('i', '<C-s>',           '<Esc>:w<CR>',	                        { noremap = true })	-- save with control-s
+map('n', '<C-s>',           ':w<CR>',		                        { noremap = true })	-- save with control-s
+map('n', '<leader>fo',      ':Neoformat<CR>',                           { noremap = true })	-- formatting
+map('n', '<leader>m',       ':MaximizerToggle<CR>',                     { noremap = true })	-- maximized buffer
 
 -- spelling bindings
-map('n', '<leader>us', ':setlocal spell! spelllang=en<CR>',         { noremap = true })     -- check english spelling
-map('n', '<leader>se', ':setlocal spell! spelllang=se<CR>',         { noremap = true })     -- check swedish spelling
+map('n', '<leader>us',      ':setlocal spell! spelllang=en<CR>',        { noremap = true })     -- check english spelling
+map('n', '<leader>se',      ':setlocal spell! spelllang=se<CR>',        { noremap = true })     -- check swedish spelling
 
 -- easymotion
-map('n', '<space>j', '<Plug>(easymotion-j)',                        { noremap = true })     -- go down
-map('n', '<space>k', '<Plug>(easymotion-k)',                        { noremap = true })     -- go up
+map('n', '<space>j',        '<Plug>(easymotion-j)',                     { noremap = true })     -- go down
+map('n', '<space>k',        '<Plug>(easymotion-k)',                     { noremap = true })     -- go up
 
 -- vimspector
-map('n', '<leader>dl', ':call vimspector#Continue()<CR>',           { noremap = true })
-map('n', '<leader>ds', ':call vimspector#Reset()<CR>',              { noremap = true })
-map('n', '<leader>b', ':call vimspector#ToggleBreakpoint()<CR>',    { noremap = true })
-map('n', '<leader>dr', ':call vimspector#Restart()<CR>',            { noremap = true })
-map('n', '<leader>e', ':call vimspector#BalloonEval()<CR>',         { noremap = true })
+map('n', '<leader>dl',          ':call vimspector#Continue()<CR>',      { noremap = true })
+map('n', '<leader>ds',      ':call vimspector#Reset()<CR>',             { noremap = true })
+map('n', '<leader>b',       ':call vimspector#ToggleBreakpoint()<CR>',  { noremap = true })
+map('n', '<leader>dr',      ':call vimspector#Restart()<CR>',           { noremap = true })
+map('n', '<leader>e',       ':call vimspector#BalloonEval()<CR>',       { noremap = true })
