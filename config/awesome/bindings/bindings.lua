b480345ff2060f41706c4f57eb57f143c36fca64
glassinatorn <gusten1995@gmail.com>
2021-07-14 01:17:47 +0200

    Removed mouse bindings for taglist I don't use

--- config/awesome/bindings/bindings.lua
+++ config/awesome/bindings/bindings.lua
@@ -122,50 +122,7 @@ for i = 1, 9 do
     )
 end
 
--- Create a wibox for each screen 
-local taglist_buttons =
-    gears.table.join(
-    awful.button(
-        {},
-        1,
-        function(t)
-            t:view_only()
-        end
-    ),
-    awful.button(
-        {vars.MODKEY},
-        1,
-        function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button(
-        {vars.MODKEY},
-        3,
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function(t)
-            awful.tag.viewnext(t.screen)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function(t)
-            awful.tag.viewprev(t.screen)
-        end
-    )
-)
+local taglist_buttons = awful.button({}, 1, function(t) t:view_only() end)
 
 -- Clientbuttons
 Clientbuttons =
@@ -189,13 +146,7 @@ Clientbuttons =
 -- {{{ Mouse bindings
 root.buttons(
     gears.table.join(
-        awful.button(
-            {},
-            3,
-            function()
-                Mymainmenu:toggle()
-            end
-        ),
+        awful.button( {}, 3, function() Mymainmenu:toggle() end),
         awful.button({}, 4, awful.tag.viewnext),
         awful.button({}, 5, awful.tag.viewprev)
     )
