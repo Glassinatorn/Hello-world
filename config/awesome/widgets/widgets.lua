45ebcec809044ba2f2e7876f9d5cf8d2724f17d0
glassinatorn <gusten1995@gmail.com>
2021-07-15 16:03:21 +0200

    Removed taglist_buttons from widgets

--- config/awesome/widgets/widgets.lua
+++ config/awesome/widgets/widgets.lua
@@ -34,54 +34,9 @@ local clock = {
     valign = "center",
 }
 
--- buttons for the taglist
-local taglist_buttons =
-    gears.table.join(
-    awful.button(
-        {},
-        1,
-        function(t)
-            t:view_only()
-        end
-    ),
-    awful.button(
-        {vars.MODKEY},
-        1,
-        function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button(
-        {vars.MODKEY},
-        3,
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function(t)
-            awful.tag.viewnext(t.screen)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function(t)
-            awful.tag.viewprev(t.screen)
-        end
-    )
-)
 
 
 return { 
     mysides = mysides,
     clock = clock,
-    taglist_buttons = taglist_buttons,
 }
