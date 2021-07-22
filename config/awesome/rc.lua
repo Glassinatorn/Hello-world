fabf198f086405793dd6145f8e0d86cbc140f4b0
glassinatorn <gusten1995@gmail.com>
2021-07-22 19:46:53 +0200

    Removed imports in rc that are not used in it

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -23,12 +23,9 @@ pcall(require, "luarocks.loader") -- luarocks
 -- importing aweomsewm libraries
 require("awful.autofocus")
 require("awful.hotkeys_popup.keys")
-local awful = require("awful")
-local menubar = require("menubar")
 local beautiful = require("beautiful")
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/glass/theme.lua")
 
-local vars = require("main.variables")      -- variables
 local rules = require("main.rules")         -- rules
 local layouts = require("main.layouts")     -- layouts
 local bindings = require("bindings.bindings")                -- keybindings
